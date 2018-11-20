1a000b962fa25203f33e316c4419db76b5e025ee
Julian Claus <kontakt@julian-claus.de>
2018-11-20 18:40:10 +0100

    chore: first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <v-app>
+    <v-btn @click="toggle">Toggle</v-btn>
+
+    <v-progress-circular
+      v-if="isVisibleProgress"
+      indeterminate
+      color="green">
+    </v-progress-circular>
+  </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    isVisibleProgress: false
+  }),
+
+  methods: {
+    toggle () {
+      this.isVisibleProgress = !this.isVisibleProgress
+    }
+  }
+}
+</script>
